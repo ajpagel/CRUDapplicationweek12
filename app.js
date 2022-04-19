582f2e9d47a8e4b4f982495862a5d603cfdb84f3
Your Name <you@example.com>
2022-04-19 15:02:54 -0500

    started adding outline

---
+++ app.js
@@ -0,0 +1,77 @@
+//add code that we want the app to do
+//class for Recipe
+//class for ingredients
+//class for recipe service that enables us to send http request to preexisting api
+//class for DOM... clear out the page
+
+
+
+
+class Recipe {
+    constructor(name) {
+        this.name;
+        this.ingredients = [];
+    }
+
+
+    //method for adding ingredients
+
+    addIngredient(name, amount){
+        this.ingredients.push(new Recipe(name, amount));
+    }
+}
+
+
+class Ingredient {
+    constructor(name, amount) {
+        this.name = name;
+        this.amount= amount;
+    }
+}
+
+
+class RecipeBook { //this willsend HTTP request
+    static url = "https://crudcrud.com/api/717ccefe42254ba59de32c272d75be1f";
+
+    //create methods and return so we can use these methods and the promise that comes back
+
+
+    //FIRST METHOD: get all recipes
+    //SECOND METHOD: get one recipe
+    //THIRD METHD: CREATE RECIPE
+    //4th: UPDATE RECIPE
+    //5th: DELETE RECIPE
+
+    
+
+}
+
+
+
+
+//then use recipebook class in DOM manager class
+//rerender the dom when we create a new class
+class DOMManager {
+
+}
+
+
+
+
+
+
+
+
+
+$('#create-new-recipe').click(() => {
+    DOMManager.createRecipe($('#new-recipe-name').val());
+    $('#new-recipe-name').val(' ');
+})
+
+//add buttons delete recipe, add ingredient, delete ingredient; add above the render of recipes
+
+
+
+//TEST
+
+DOMManager.getAllRecipes();
